e766c5e03fdce3de3cddda4e7ff6604638c02aa0
olivernyc <oliver@static.network>
2019-02-21 00:01:48 -0500

    Fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "tachyons";
 import "./index.css";
 import App from "./App";
-if (process.env.NODE_ENV === "development") require("tachyons");
 
 ReactDOM.render(<App />, document.getElementById("map-root"));
