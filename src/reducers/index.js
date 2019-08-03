c35e78826b98755f693641c95f7c85e17e9be526
Oliver Fisher <oliver@static.network>
2019-08-03 16:22:31 -0400

    Rename vpn to remote (#19)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,7 +6,7 @@ import sectorData from "../data/sectors";
 import kiosks from "../data/kiosks";
 
 const initialFilters = {
-	vpn: true,
+	remote: true,
 	linkNYC: false,
 	potential: false,
 	dead: false,
