4e2b4e6ba839a1aafa19c7c78a6d3c30c7af0668
Andrew Dickinson <andrewjd@amazon.com>
2024-03-18 00:07:25 -0400

    Fix: CSS styles are missing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-if (process.env.NODE_ENV === "development") {
-	require("tachyons");
-	require("./index.css");
-} else {
-	require("./index.css");
-}
+
+require("tachyons");
+require("./index.css");
+
 
 ReactDOM.render(<App />, document.getElementById("map-root"));
